713d0388bd863d94797783d5e974e248653e648e
harshmangalam <harshdev8218@gmail.com>
2022-10-05 20:39:57 +0530

    added signup validation middleaware

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body } = require("express-validator");
 const authRouter = express.Router();
 
 const {
@@ -8,7 +9,18 @@ const {
   restPasswordController,
 } = require("../controller/authController");
 
-authRouter.post("/signup", signUpController);
+const signupValidation = [
+  body("name").not().isEmpty().withMessage("Name must be required"),
+  body("email")
+    .not()
+    .isEmpty()
+    .withMessage("Email address must be required")
+    .isEmail()
+    .withMessage("Incorrect email address"),
+  body("password").not().isEmpty().withMessage("Password must be required"),
+];
+
+authRouter.post("/signup", signupValidation, signUpController);
 authRouter.post("/login", loginController);
 authRouter.patch("/forgotPassword", forgotPasswordController);
 authRouter.patch("/restPassword", restPasswordController);
