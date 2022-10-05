0832b089269ace3b93b5f8571bb9612f7d8e13db
harshmangalam <harshdev8218@gmail.com>
2022-10-05 20:39:28 +0530

    added express json parser

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRouter");
 const connectDB = require("./DB/connect");
-const { notFoundErrorHandler, globalErrorHandler } = require("./middlewares/errorHandler");
+const {
+  notFoundErrorHandler,
+  globalErrorHandler,
+} = require("./middlewares/errorHandler");
+
+// pasrse incomming request data as json
+app.use(express.json());
+
 
 app.use("/api/v1/auth", authRouter);
 
