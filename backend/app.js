d71e641877e228b76a5f27af092de15a56a5f023
harshmangalam <harshdev8218@gmail.com>
2022-10-05 20:02:46 +0530

    added not found handler

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const authRouter = require('./routes/authRouter');
+const authRouter = require("./routes/authRouter");
 const connectDB = require("./DB/connect");
+const { notFoundErrorHandler } = require("./middlewares/errorHandler");
 
-app.use('api/v1/auth' , authRouter);
+app.use("api/v1/auth", authRouter);
+
+app.use("*", notFoundErrorHandler); // api route not found error handling
 
 // server and DB
 const port = 3000;
-const start = ()=>{
-    try{
-       connectDB();
-       app.listen(port, console.log(`Server listening at port ${port}`));
-    }catch(error){
-        console.log(error);
-    }
-}
+const start = () => {
+  try {
+    connectDB();
+    app.listen(port, console.log(`Server listening at port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 start();
