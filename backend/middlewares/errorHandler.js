5b75781b51bcdc2f859e7bae25a37ff4857f91f6
harshmangalam <harshdev8218@gmail.com>
2022-10-05 20:03:53 +0530

    added global error handler

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -5,3 +5,15 @@ exports.notFoundErrorHandler = (req, res, next) => {
   };
   next(error);
 };
+
+exports.globalErrorHandler = (err, req, res, next) => {
+  console.log(err);
+  const status = err.status || 500;
+  const message = err.message || "Internal server error";
+  const data = err.data || null;
+  res.status(status).json({
+    type: "error",
+    message,
+    data,
+  });
+};
