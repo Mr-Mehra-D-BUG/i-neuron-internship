620aeeaf137353842c41c50ce9d160599041a4f8
harshmangalam <harshdev8218@gmail.com>
2022-10-05 20:04:44 +0530

    added middleware to handle global error

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const authRouter = require("./routes/authRouter");
 const connectDB = require("./DB/connect");
-const { notFoundErrorHandler } = require("./middlewares/errorHandler");
+const { notFoundErrorHandler, globalErrorHandler } = require("./middlewares/errorHandler");
 
 app.use("api/v1/auth", authRouter);
 
 app.use("*", notFoundErrorHandler); // api route not found error handling
-
+app.use(globalErrorHandler); //global error handler
 // server and DB
 const port = 3000;
 const start = () => {
