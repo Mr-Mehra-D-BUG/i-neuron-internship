d35bdbf7c3c51b7e97fc664f39af7cd9e47d4d0b
Gaurav_Singh <gauravkarki038@gmail.com>
2022-10-15 19:10:04 +0530

    HomePage

---
+++ frontside/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Categories from '../Components/Categories'
+import Slider from '../Components/Slider'
+
+function Home() {
+  return (
+    <div>
+        <Slider />
+        <Categories />
+    </div>
+  )
+}
+
+export default Home
